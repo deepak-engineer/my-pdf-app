bbf12d8667227b4ae6ee611058f4670efe9e1a63
deepak-engineer <devwebwithdeepak@gmail.com>
2025-08-30 15:53:49 +0530

    Initial commit from Create Next App

---
+++ src/app/layout.js
@@ -0,0 +1,18 @@
+import "./globals.css";
+import Navbar from "../components/Navbar";
+
+export const metadata = {
+  title: "PDF Tools",
+  description: "iLovePDF Clone built with Next.js App Router",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="bg-black text-white">
+        <Navbar />
+        <main className="p-6">{children}</main>
+      </body>
+    </html>
+  );
+}
